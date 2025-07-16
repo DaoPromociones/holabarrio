0836c7699d28904ef4025dc7961075ac93381c39
macala <manuelxcalatrava@gmail.com>
2025-07-16 09:02:13 +0200

    Actualizar nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,8 @@ const nextConfig = {
   // De momento lo dejamos vacío,
   // aquí iría configuración futura si la necesitamos.
   reactStrictMode: true,
+  // Activa la salida optimizada para Docker
+  output: 'standalone',
 };
 
 export default nextConfig;
